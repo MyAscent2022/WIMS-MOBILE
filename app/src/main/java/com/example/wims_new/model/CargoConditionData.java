fef743108dc143f60094bbd149a402bb007b3424
Carlo <carlo090899@gmail.com>
2023-11-09 13:40:03 +0800

    Receive Cargo (Capture Image)

---
+++ app/src/main/java/com/example/wims_new/model/CargoConditionData.java
@@ -0,0 +1,16 @@
+package com.example.wims_new.model;
+
+import java.util.List;
+
+public class CargoConditionData {
+
+    List<CargoConditionModel> condition;
+
+    public List<CargoConditionModel> getCondition() {
+        return condition;
+    }
+
+    public void setCondition(List<CargoConditionModel> condition) {
+        this.condition = condition;
+    }
+}
